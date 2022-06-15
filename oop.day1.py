55f5bb2fbdc64e5609339502114382f2521a4a23
Zan <ozankaze101@gmail.com>
2022-06-16 06:04:29 +0700

    Pendahuluan Object Oriented Programming

---
+++ oop.day1.py
@@ -0,0 +1,27 @@
+# class Hero(): # template
+
+#     pass
+
+# hero1 = Hero() # object
+# hero2 = Hero()
+# hero3 = Hero()
+
+# hero1.name = "Fade"
+# hero1.demage = 3000
+# hero1.role = "Initiator"
+
+# hero2.name = "Neon"
+# hero2.demage = 5000
+# hero2.role = "Duelist"
+
+# hero3.name = "Jett"
+# hero3.demage = 9000
+# hero3.role = "Duelist"
+
+# # print(hero1) -> untuk mengecek bahwa dia itu object
+# print(hero1.name)
+# print(hero2.__dict__)
+# print(hero3.__dict__)
+# print(hero1.__dict__)
+
+
