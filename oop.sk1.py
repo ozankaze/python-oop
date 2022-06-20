556579936ee7b276def63e15fdf1d7c6829844c1
Zan <ozankaze101@gmail.com>
2022-06-20 20:00:40 +0700

    Sumber Belajar

---
+++ oop.sk1.py
@@ -0,0 +1,22 @@
+
+
+
+
+
+# ############
+# OOP Python
+# ############
+
+# Ini adalah sumber link belajar.
+
+# ############
+"""
+
+https://www.pythontutorial.net/python-oop/
+
+
+"""
+# ############
+
+
+# 
