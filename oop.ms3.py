f44a440d367119ac77d6e7dcd58087e504d1b2f7
Zan <ozankaze101@gmail.com>
2022-06-25 11:53:26 +0700

    EPS 3- Constructors

---
+++ oop.ms3.py
@@ -0,0 +1,15 @@
+# ==== EPS 3- Constructors
+
+class Point():
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def draw(self):
+        print(f"Point ({self.x}, {self.y})")
+
+point = Point(1, 2)
+# print(point.x)
+print(point.draw())
+
